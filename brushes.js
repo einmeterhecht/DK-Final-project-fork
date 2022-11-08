bb29a2a6aed2f6ff19fa38223cd07aa26112c754
Damian <damian.keller-li@bluewin.ch>
2022-11-08 18:55:04 +0100

    brush

--- brushes.js
+++
@@ -1,8 +0,0 @@
-function aquarellBrush(thickness, len) {
-  strokeWeight(thickness)
-  for (i = 0; i < len; i++) {
-    strokeWeight(noise(i / 10) * thickness)
-    stroke(0, 255 * noise(i + 3454))
-    point(noise(i + 2334) * 2, i)
-  }
-}
